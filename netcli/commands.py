095eb3835c1b251d6f101f07ef8367aacb1acbde
QuantumEnergyE <quantumenergye@gmail.com>
2018-02-27 13:43:25 +0800

    add bond to bridge

--- netcli/commands.py
+++ netcli/commands.py
@@ -16,17 +16,17 @@ class NetWorkCommands(object):
     base_if_path = "/etc/sysconfig/network-scripts/"
     route_file = '/etc/sysconfig/network-scripts/route-'
     func_map = {
-        'list ips': 'list_ips',
+        'list ifs': 'list_ifs',
         'list routes': 'list_routes',
         'list cards': 'list_cards',
-        'get ip': 'get_ip',
+        'get if': 'get_if',
         'get route': 'get_route',
-        'set ifcfg': 'set_ifcfg',
+        'set if': 'set_if',
         'set route': 'set_route',
         'set ns': 'set_ns',
         'set bond': 'set_bond',
         'set bridge': 'set_bridge',
-        'del ip': 'del_ip',
+        'del if': 'del_if',
         'del route': 'del_route',
         "restart network": 'restart_network'
     }
@@ -62,7 +62,7 @@ class NetWorkCommands(object):
             raise NotImplementedError('not implemented!')
         return func
 
-    def list_ips(self):
+    def list_ifs(self):
         out, error, code = self.run_shell('ifconfig -a')
         self.secho(out, error, code, pager=True)
 
@@ -74,7 +74,7 @@ class NetWorkCommands(object):
         out, error, code = self.run_shell('ls /sys/class/net/')
         self.secho(out, error, code)
 
-    def get_ip(self):
+    def get_if(self):
         eth = get_input('please input the eth name:')
         out, error, code = self.run_shell('ifconfig {}'.format(eth))
         self.secho(out, error, code)
@@ -89,26 +89,64 @@ class NetWorkCommands(object):
             routes = r.read()
             click.secho(routes, fg='green')
 
-    def set_ifcfg(self):
+    def set_if(self):
         eth = get_input('network card:')
-        mode = get_input('select the mode [dhcp(d)/static(s)/bond(b)]/bridge(e):')
+        mode = get_input('select the mode [dhcp(d)/static(s)/bond(b)/bridge(e)]:')
         if mode == 'static' or mode == 's':
             ip = get_input('ip:')
             mask = get_input('mask:')
             gateway = get_input('gateway:')
-            self.set_ip_static(eth, ip, mask, gateway)
+            self.set_if_static(eth, ip, mask, gateway)
         elif mode == 'dhcp' or mode == 'd':
-            self.set_ip_dhcp(eth)
+            self.set_if_dhcp(eth)
         elif mode == 'bond' or mode == 'b':
             bond = get_input('bond name:')
-            nm_controlled = get_input('nm_controlled [yes/no]:')
-            self.set_ip_bond(eth, bond, nm_controlled)
+            self.add_if_to_bond(eth, bond)
         elif mode == 'bridge' or mode == 'e':
-            bridge = get_input('bridge name, If this option is empty, '
-                               'the bridge configuration of the network card will be deleted:')
+            bridge = get_input('bridge name:')
             self.add_if_to_bridge(eth, bridge)
         else:
             click.secho('Please select an available option!', fg='red')
+
+    def set_if_static(self, eth, ip, mask, gateway):
+        info = 'DEVICE={}\n' \
+               'BOOTPROTO=static\n' \
+               'ONBOOT=yes\n' \
+               'IPADDR={}\n' \
+               'NETMASK={}\n' \
+               'GATEWAY={}'.format(eth, ip, mask, gateway)
+        self.update_ifcfg(eth, info)
+
+    def set_if_dhcp(self, eth):
+        info = 'DEVICE={}\n' \
+               'BOOTPROTO=dhcp\n' \
+               'ONBOOT=yes'.format(eth)
+        self.update_ifcfg(eth, info)
+
+    def add_if_to_bond(self, slave, bond):
+        info = 'MASTER={}\n' \
+               'DEVICE={}\n' \
+               'ONBOOT=yes\n' \
+               'SLAVE=yes\n' \
+               'NM_CONTROLLED=no'.format(bond, slave)
+        self.update_ifcfg(slave, info)
+
+    def add_if_to_bridge(self, eth, bridge):
+        info = 'DEVICE={}\n' \
+               'BOOTPROTO=none\n' \
+               'ONBOOT=yes\n' \
+               'BRIDGE={}'.format(eth, bridge)
+        self.update_ifcfg(eth, info)
+
+    def update_ifcfg(self, device, info):
+        click.secho('configuration information:\n{}'.format(info), fg='yellow')
+        ret = get_input('Please confirm the above information [y/n]:')
+        if ret.lower() == 'y':
+            with open(os.path.join(self.base_if_path, 'ifcfg-{}'.format(device)), 'w') as w:
+                w.write(info)
+            click.secho('successful!', fg='green')
+        else:
+            click.secho('cancel!', fg='green')
 
     def set_route(self):
         net = get_input('net:')
@@ -128,44 +166,83 @@ class NetWorkCommands(object):
         pass
 
     def set_bond(self):
-        bond = get_input('bond device [example=>bond0]:')
         cards, _, _ = self.run_shell('ls /sys/class/net/')
-        mode = get_input('bond mode:')
+        bond = get_input('bond device [example=>bond0]:')
+        bond_mode = get_input('bond mode:')
         miimon = get_input('bond miimon:')
-        ipaddr = get_input('ipaddr:')
-        netmask = get_input('netmask:')
         onboot = get_input('onboot [yes/no]:')
-        mtu = get_input('mtu:')
-        nm_controlled = get_input('nm_controlled:')
+        mode = get_input('select the mode [dhcp(d)/static(s)//bridge(e)]:')
+
+        if mode == 'dhcp' or mode == 'd':
+            self.set_bond_dhcp(bond, bond_mode, miimon, onboot)
+        elif mode == 'bridge' or mode == 'e':
+            bridge = get_input('bridge name:')
+            self.add_bond_to_bridge(bond, bond_mode, miimon, onboot, bridge)
+        else:
+            ipaddr = get_input('ipaddr:')
+            netmask = get_input('netmask:')
+            gateway = get_input('gateway:')
+            self.set_bond_static(bond, bond_mode, miimon, ipaddr, netmask, gateway, onboot)
+
+    def set_bond_static(self, bond, bond_mode, miimon, ipaddr, netmask, gateway, onboot):
         info = 'DEVICE={}\n' \
+               'BOOTPROTO=static\n' \
                'BONDING_OPTS=\'mode={} mimmon={}\'\n' \
                'IPADDR={}\n' \
                'NETMASK={}\n' \
-               'ONBOOT=yes\n' \
-               'MTU={}\n' \
-               'NM_CONTROLLED={}\n' \
-               'HOTPLUG={}'.format(bond, mode, miimon, ipaddr, netmask, onboot, mtu, nm_controlled)
-        click.secho('configuration information:\n'
-                    '{}'.format(info), fg='yellow')
-        ret = get_input('Please confirm the above information [y/n]:')
-        if ret.lower() == 'y':
-            bond_file = os.path.join(self.base_if_path, 'ifcfg-{}'.format(bond))
-            if os.path.exists(bond_file):
-                ret = get_input('bond conf ifcfg-{} is exist, do you want to recover it! [y/n]:'.format(bond))
-                if ret.lower() != 'y':
-                    return
-            with open(bond_file, 'w') as w:
-                w.write(info)
-            ret = get_input('do you want bind devices to {} [y/n]:'.format(bond))
-            if ret.lower() == 'y':
-                slave_devices = get_input('please select the slave devices from "{}" [example=>ens3 ens4]:'.format(
-                    ';'.join(cards.split('\n'))))
-                for slave_device in slave_devices.split():
-                    self.set_ip_bond(slave_device, bond)
-            else:
-                click.secho('you can bind device to bond by execute \'set ifcfg\'', fg='green')
+               'GATEWAY={}\n' \
+               'ONBOOT={}\n' \
+               'NM_CONTROLLED=no\n' \
+               'HOTPLUG=no'.format(bond, bond_mode, miimon, ipaddr, netmask, gateway, onboot)
+        self.update_ifcfg(bond, info)
 
-    def del_ip(self):
+    def set_bond_dhcp(self, bond, bond_mode, miimon, on_boot):
+        info = 'DEVICE={}\n' \
+               'BOOTPROTO=dhcp\n' \
+               'BONDING_OPTS=\'mode={} mimmon={}\'\n' \
+               'ONBOOT={}\n' \
+               'NM_CONTROLLED=no\n' \
+               'HOTPLUG=no'.format(bond, bond_mode, miimon, on_boot)
+        self.update_ifcfg(bond, info)
+
+    def add_bond_to_bridge(self, bond, bond_mode, miimon, on_boot, bridge):
+        info = 'DEVICE={}\n' \
+               'BONDING_OPTS=\'mode={} mimmon={}\'\n' \
+               'ONBOOT={}\n' \
+               'NM_CONTROLLED=no\n' \
+               'HOTPLUG=no\n' \
+               'BRIDGE={}'.format(bond, bond_mode, miimon, on_boot, bridge)
+        self.update_ifcfg(bond, info)
+
+    def set_bridge(self):
+        bridge = get_input('bridge device [example=>br0]:')
+        mode = get_input('[dhcp(d)/static(s)]:')
+        if mode == 'dhcp' or mode == 'd':
+            self.set_bridge_dhcp(bridge)
+        else:
+            ip = get_input('ip:')
+            mask = get_input('mask:')
+            gateway = get_input('gateway:')
+            self.set_bridge_static(bridge, ip, mask, gateway)
+
+    def set_bridge_static(self, bridge, ip, mask, gateway):
+        info = 'device={}\n' \
+               'TYPE=Bridge\n' \
+               'BOOTPROTO=static\n' \
+               'ONBOOT=yes\n' \
+               'IPADDR={}\n' \
+               'NETMASK={}\n' \
+               'GATEWAY={}'.format(bridge, ip, mask, gateway)
+        self.update_ifcfg(bridge, info)
+
+    def set_bridge_dhcp(self, bridge):
+        info = 'device={}\n' \
+               'TYPE=Bridge\n' \
+               'BOOTPROTO=dhcp\n' \
+               'ONBOOT=yes'.format(bridge)
+        self.update_ifcfg(bridge, info)
+
+    def del_if(self):
         pass
 
     def del_route(self):
@@ -186,93 +263,11 @@ class NetWorkCommands(object):
         else:
             click.secho('can\'t find route!', fg='red')
 
-    def set_ip_static(self, eth, ip, mask, gateway):
-        info = 'DEVICE={}\n' \
-               'BOOTPROTO=static\n' \
-               'ONBOOT=yes\n' \
-               'IPADDR={}\n' \
-               'NETMASK={}\n' \
-               'GATEWAY={}'.format(eth, ip, mask, gateway)
-        self.update_ifcfg(eth, info)
-
-    def set_ip_dhcp(self, eth):
-        info = 'DEVICE={}\n' \
-               'BOOTPROTO=dhcp\n' \
-               'ONBOOT=yes'.format(eth)
-        self.update_ifcfg(eth, info)
-
-    def set_ip_bond(self, slave, bond, nm_controlled='no'):
-        info = 'MASTER={}\n' \
-               'DEVICE={}\n' \
-               'ONBOOT=yes\n' \
-               'SLAVE=yes\n' \
-               'NM_CONTROLLED={}'.format(bond, slave, nm_controlled)
-        self.update_ifcfg(slave, info)
-
-    def add_if_to_bridge(self, eth, bridge):
-        eth_file = os.path.join(self.base_if_path, 'ifcfg-{}'.format(eth))
-        if not os.path.exists(eth_file):
-            click.secho('can\'t find configuration file of {}, please set ifcfg with dhcp or static first!'.format(eth))
-        if not bridge.strip():
-            click.secho('network card {} will be removed from bridge'.format(eth))
-            command = 'sed -i \'/^{}/d\' {}'.format('BRIDGE', eth_file)
-            print(command)
-            out, error, code = self.run_shell(command)
-            self.secho(out, error, code)
-            return
-        ret = get_input('interface {} will be added to bridge {} [y/n]:'.format(eth, bridge))
-        if ret.lower() == 'y':
-            #TODO
-            out, error, code = self.run_shell('sed -i \'s/^BRIDGE.*$/BRIDGE={}\' {}'.format(bridge, eth_file))
-            self.secho(out, error, code)
-
-    def update_ifcfg(self, device, info):
-        click.secho('configuration information:\n{}'.format(info), fg='yellow')
-        ret = get_input('Please confirm the above information [y/n]:')
-        if ret.lower() == 'y':
-            with open(os.path.join(self.base_if_path, 'ifcfg-{}'.format(device)), 'w') as w:
-                w.write(info)
-            click.secho('successful!', fg='green')
-        else:
-            click.secho('cancel!', fg='green')
-
     def restart_network(self):
         ret = get_input('The network service will be restarted! [y/n]:')
         if ret.lower() == 'y':
             out, error, code = self.run_shell('systemctl restart network')
             self.secho(out, error, code)
-
-    def set_bridge(self):
-        bridge = get_input('bridge device [example=>br0]:')
-        mode = get_input('[dhcp(d)/static(s)]:')
-        if mode == 'dhcp' or mode == 'd':
-            info = 'device={}\n' \
-                   'TYPE=Bridge\n' \
-                   'BOOTPROTO=dhcp\n' \
-                   'ONBOOT=yes'.format(bridge)
-        else:
-            ip = get_input('ip:')
-            mask = get_input('mask:')
-            gateway = get_input('gateway:')
-            info = 'device={}\n' \
-                   'TYPE=Bridge\n' \
-                   'BOOTPROTO=static\n' \
-                   'ONBOOT=yes\n' \
-                   'IPADDR={}\n' \
-                   'NETMASK={}\n' \
-                   'GATEWAY={}'.format(bridge, ip, mask, gateway)
-        click.secho('configuration information:\n'
-                    '{}'.format(info), fg='yellow')
-        ret = get_input('Please confirm the above information [y/n]:')
-        if ret.lower() == 'y':
-            bridge_file = os.path.join(self.base_if_path, 'ifcfg-{}'.format(bridge))
-            if os.path.exists(bridge_file):
-                ret = get_input('bridge conf ifcfg-{} is exist, do you want to recover it! [y/n]:'.format(bridge))
-                if ret.lower() != 'y':
-                    return
-            with open(bridge_file, 'w') as w:
-                w.write(info)
-            click.secho('Successful, you can add device to the bridge by execute \'set ifcfg\'', fg='green')
 
     def run_cmd(self, text):
         self.parse(text)()
