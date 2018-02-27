65629b82951f9b48a74a0992744a209dbda64ce2
QuantumEnergyE <quantumenergye@gmail.com>
2018-02-27 10:19:40 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='blog_quantum',
+    version='1.0',
+    packages=['blog'],
+    include_package_data=True,
+    install_requires=[
+        'pygments>=2.0.2',
+        'prompt_toolkit>=1.0.0,<1.1.0',
+        'click>=4.0',
+        'fuzzyfinder>=1.0.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'netcli = netcli.main:cli',
+        ]
+    },
+    url='https://github.com/QuantumEnergyE/netcli',
+    author='quantum',
+    author_email='quantumenergye@gmail.com ',
+)
